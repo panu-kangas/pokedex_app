382e99cc083992396f15400a4e30193a37f5d557
Panu Kangas <kangas.panu@gmail.com>
2025-04-14 14:06:19 +0300

    fixing CORS error

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(cors());
+app.use(cors({
+	origin: 'https://pokedex-app-seven-tan.vercel.app/',
+}));
 
 mongoose.connect(MONGO_URI)
 	.then(() => console.log('\n\n🧠 Connected to MongoDB'))
