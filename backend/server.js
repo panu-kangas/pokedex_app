2013bd3878b5632c08283377efea7d3fc6eb50c0
Panu Kangas <kangas.panu@gmail.com>
2025-04-14 14:15:35 +0300

    Cors fix vol 2

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors({
-	origin: 'https://pokedex-app-seven-tan.vercel.app/',
+	origin: 'https://pokedex-app-seven-tan.vercel.app',
 }));
 
 mongoose.connect(MONGO_URI)
